0cfb1c6f6fafd18996fa232cc65d57947af47f29
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-11 09:02:19 +0530

    explore menu component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
+import ExpoloreMenu from "../../components/ExploreMenu/ExpoloreMenu";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
+      <ExpoloreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
