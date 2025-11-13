c0c8d2f60ad2490aec7e7e72674721516d27d225
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-13 08:36:53 +0530

    feat: set up Express server with CORS and JSON middleware

--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,18 @@
- 
+import express from "express";
+import cors from "cors";
+
+//app config
+const app = express();
+const port = 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
